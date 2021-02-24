8d6c0fcdcb06fdb318308c38d282ed3aa987432b
Alex <alex0692pash@mail.ru>
2021-02-24 22:34:11 +0300

    005 init

---
+++ 005_Double/cmake-build-debug/CMakeFiles/005_Double.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "005_Double.exe"
+  "005_Double.exe.manifest"
+  "005_Double.pdb"
+  "CMakeFiles/005_Double.dir/main.cpp.obj"
+  "lib005_Double.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/005_Double.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 005_Double/cmake-build-debug/CMakeFiles/005_Double.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 005_Double.
+# This may be replaced when dependencies are built.
