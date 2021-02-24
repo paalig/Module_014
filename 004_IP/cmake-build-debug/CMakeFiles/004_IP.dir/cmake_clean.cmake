d60c2b10ab3f387e1679feb7fff698b48315bf6c
Alex <alex0692pash@mail.ru>
2021-02-24 22:23:42 +0300

    004 init

---
+++ 004_IP/cmake-build-debug/CMakeFiles/004_IP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "004_IP.exe"
+  "004_IP.exe.manifest"
+  "004_IP.pdb"
+  "CMakeFiles/004_IP.dir/main.cpp.obj"
+  "lib004_IP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/004_IP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 004_IP/cmake-build-debug/CMakeFiles/004_IP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 004_IP.
+# This may be replaced when dependencies are built.
