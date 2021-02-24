bc09dc8c7b96fddc28b852d94345ec0966506374
Alex <alex0692pash@mail.ru>
2021-02-24 22:11:14 +0300

    001 init

---
+++ 001_Caesar/cmake-build-debug/CMakeFiles/001_Caesar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "001_Caesar.exe"
+  "001_Caesar.exe.manifest"
+  "001_Caesar.pdb"
+  "CMakeFiles/001_Caesar.dir/main.cpp.obj"
+  "lib001_Caesar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/001_Caesar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
