2bfb03848890246daa8f1fd856ed9df5dca53131
Alex <alex0692pash@mail.ru>
2021-02-24 22:20:46 +0300

    003 init

---
+++ 003_Email/cmake-build-debug/CMakeFiles/003_Email.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "003_Email.exe"
+  "003_Email.exe.manifest"
+  "003_Email.pdb"
+  "CMakeFiles/003_Email.dir/main.cpp.obj"
+  "lib003_Email.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/003_Email.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
