cd76cfbd5b3aa21df235bc0a2cc7ffa0d241182e
Alex <alex0692pash@mail.ru>
2021-02-25 23:36:02 +0300

    001 done

--- 001_Caesar/main.cpp
+++ 001_Caesar/main.cpp
@@ -14,7 +14,64 @@
  * Подсказка: имея функцию encryptCaesar, функцию decryptCaesar можно реализовать в одну строчку.
  */
 
+bool Check (std::string str) {
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] < 'A' || str[i] > 'z' || (str[i] > 'Z' && str[i] < 'a')) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::string EncryptCaesar(std::string str, int a) {
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            if (str[i] + a > 'z') {
+                str[i] += a - 26;
+            } else if (str[i] + a < 'a') {
+                str[i] += a + 26;
+            } else {
+                str[i] += a;
+            }
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            if (str[i] + a > 'Z') {
+                str[i] += a - 26;
+            } else if (str[i] + a < 'A') {
+                str[i] += a + 26;
+            } else {
+                str[i] += a;
+            }
+        }
+    }
+    return str;
+}
+
+std::string DecryptCaesar(std::string str, int a) {
+    return EncryptCaesar(str, -a);
+}
+
+void PrintStr(std::string str) {
+    for (int i = 0; i < str.size(); i++) {
+        std::cout << str[i];
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::string str;
+    int a;
+
+    std::cout << "Input string to encrypt:" << std::endl;
+    std::cin >> str;
+    std::cout << "Input number for encryption:" << std::endl;
+    std::cin >> a;
+
+    if(Check(str)) {
+        std::cout << "Encrypt: ";
+        PrintStr(EncryptCaesar(str, a));
+        std::cout << std::endl;
+        std::cout << "Decrypt: ";
+        PrintStr(DecryptCaesar(str, a));
+    } else {
+        std::cout << "Wrong input";
+    }
 }
