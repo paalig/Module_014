9ba643301cf6d706858cf6c1739bd33f219d25ff
Alex <alex0692pash@mail.ru>
2021-02-28 15:33:10 +0300

    003 done

--- 003_Email/main.cpp
+++ 003_Email/main.cpp
@@ -10,8 +10,80 @@
  * Пользователь вводит строку, задающую email-адрес.
  * Программа должна вывести слово Yes, если адрес корректен, а в противном случае - слово No.
  */
+bool CheckSize (std::string str) {
+    int a = 0;
+    if (str.empty() || str.size() > 128) {
+        return false;
+    }
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '@') {
+            a = 1;
+        } else {
+            a++;
+        }
+        if (a > 64) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool CheckSymbol(std::string str) {
+    int countA = 0;
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '@') {
+            countA++;
+        }
+        //не больше одной @
+        if (countA > 1) {
+            return false;
+        }
+        // A-Z, a-z, 0-9, ".", "-", @
+        if ((!(str[i] >= 'a' && str[i] <= 'z') && !(str[i] >= 'A' && str[i] <= 'Z') &&
+            !(str[i] >= '0' && str[i] <= '9') && str[i] != '-' && str[i] != '.') && str[i] != '@') {
+            //доп.символы в первой части
+            if (countA == 0) {
+                std::string firstPartSymbol = "!#$%&'*+-/=?^_`{|}~";
+                bool firstPart = false;
+                for (int j = 0; j < firstPartSymbol.size(); j++) {
+                    if (str[i] == firstPartSymbol[j]) {
+                        firstPart = true;
+                        break;
+                    }
+                }
+                if (!firstPart) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+    //точки
+    if (str[0] == '.' || str[str.size() - 1] == '.') {
+        return false;
+    }
+    for (int i = 0; i < (str.size() - 1); i++) {
+        if (str[i] == str[i + 1] && str[i] == '.') {
+            return false;
+        }
+    }
+    //есть @ в адресе
+    if (countA < 1) {
+        return false;
+    }
+    return true;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string str;
+    std::cout << "Input email:" << std::endl;
+    std::cin >> str;
+
+    if (CheckSize(str) && CheckSymbol(str)) {
+        std::cout << "Yes" << std::endl;
+    } else {
+        std::cout << "No" << std::endl;
+    }
     return 0;
 }
